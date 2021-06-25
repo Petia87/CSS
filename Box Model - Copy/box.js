2f5d15111c24eebdcf6eb3c610810db1106787de
Petia Valchkova <peps@gmail.com>
2021-06-26 00:23:52 +0300

    box

--- Box Model - Copy/box.js
+++ Box Model - Copy/box.js
@@ -4,4 +4,17 @@ function showText() {
     document.getElementById('text')
     .style.display='inline'
   }
+
+  function more(message) {
+  
+    const element=document.getElementById('more');
+    element.style.display='block';
+    element.textContent=message;
+  
+    element.addEventListener('click', onClick);
+  
+    function onClick(event) {
+      event.target.style.display='none';
+    }
+  }
   
